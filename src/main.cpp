b8730955773299ecdd4572f72024eef6f99b6363
Meena Atai <2397995@cmontmorency.qc.ca>
2025-11-04 12:03:21 -0500

    Initialisation

---
+++ src/main.cpp
@@ -0,0 +1,53 @@
+#include <Arduino.h>
+#include <M5_PbHub.h>
+#include <MicroOscSlip.h>
+#include <FastLED.h>
+#include <VL53L0X.h>
+
+VL53L0X  myTOF;
+
+MicroOscSlip<128> monOsc(&Serial);
+M5_PbHub myPbHub;
+
+#define BROCHE_ATOM_FIL_BLANC 32 // connecté
+#define BROCHE_ATOM_PIXEL 27
+CRGB atomPixel;
+
+#define CANAL_ANGLE 0
+
+void setup() {
+  Wire.begin();
+  myPbHub.begin();
+  myTOF.init();
+
+  Serial.begin(115200);
+
+  // Initialiser FastLED pour contrôler le pixel RGB du M5Atom
+  FastLED.addLeds<WS2812, 27 , GRB >(&atomPixel, 1); 
+
+
+  atomPixel = CRGB(255,0,0); // ROUGE
+  FastLED.show();
+  delay(1000); // PAUSE 1 SECONDE
+  atomPixel = CRGB(255,255,0); // JAUNE
+  FastLED.show();
+  delay(1000); // PAUSE 1 SECONDE
+  atomPixel = CRGB(0,255,0); // VERT
+  FastLED.show();
+  delay(1000); // PAUSE 1 SECONDE
+  atomPixel = CRGB(0,0,0);
+  FastLED.show(); // PAUSE 1 SECONDE
+
+}
+
+void loop() {
+  int maLectureAngle = myPbHub.analogRead(CANAL_ANGLE);
+  monOsc.sendInt("/angle", maLectureAngle);
+
+  //exercice - TOF
+
+  uint16_t mesure = myTOF.readRangeSingleMillimeters();
+
+
+  delay(50);
+}
